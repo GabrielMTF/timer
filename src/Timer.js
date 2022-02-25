78ef3b1d1780723da2a5ffb489b8ed29d2800c80
gabriel.freitas <gabriel.freitas@grupoicts.com.br>
2022-02-25 14:12:30 -0300

    alteracao

--- src/Timer.js
+++ src/Timer.js
@@ -2,13 +2,27 @@ import React, { useState, useEffect } from 'react';
 
 import './Timer.css';
 
-function Timer(duration, display) {
+function Timer() {
 
-  const [clock, setClock] = useState('00:00')
+  const total = 600;
+
+  let minutes = Math.floor(total % 3600 / 60);
+  let seconds = Math.floor(total % 3600 % 60);
+
+  const [clock, setClock] = useState(minutes + ":" + seconds)
+
+  useEffect(() => {
+    clock > 0 && setTimeout(() => setClock(clock - 1), 1000);
+    return () => clearInterval(clock);
+  }, [clock]);
 
   return (
     <div className='container'>
-      {clock}
+
+      <div>
+        {clock}
+      </div>
+      <button>RESET</button>
     </div>
   )
 }
